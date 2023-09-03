8065c3cdc833b7550a69efdf7744c7d000bb42ae
Prannaya <prannayagupta@gmail.com>
2023-09-04 02:56:08 +0800

    feat(amlp): actually implement aMLP

--- layers/gmlp.py
+++ layers/gmlp.py
@@ -1,44 +1,87 @@
+"""
+gMLP (Gated Multi-Layer Perceptron) Implementation
+
+Based on the Implementation Proposed by "Pay Attention to MLPs" by Hanxiao Liu, Zihang Dai, David R. So, Quoc V. Le
+(NeurIPS 2021).
+
+P.S. aMLP has also been implemented here since it's from the same paper.
+"""
+
 from keras_core import ops, layers, activations, Sequential
 from typing import Tuple
 from utils.types import Int, Float, TensorLike
 
-__all__ = ["SpatialGatingUnit", "gMLP"]
+__all__ = ["SpatialGatingUnit", "gMLPBlock", "TinyAttention"]
 
 
 class SpatialGatingUnit(layers.Layer):
-    def __init__(self, units, eps: Float = 1e-6, **kwargs):
-        super(SpatialGatingUnit, self).__init__(**kwargs)
+    def __init__(self, eps: Float = 1e-6, use_attention: bool = False, d_attn: Int = 64, **kwargs):
+        super().__init__(**kwargs)
+        self.use_attention = use_attention
+        self.d_attn = d_attn
         self.norm = layers.LayerNormalization(epsilon=eps)
-        self.proj = layers.Dense(units, bias_initializer="Ones")
+        self.proj = None
+        self.att = None
+
+    def build(self, input_shape: Tuple[Int]):
+        self.proj = layers.Dense(input_shape[1], bias_initializer="Ones")
+        if self.use_attention:
+            self.att = TinyAttention(input_shape[-1] // 2, d_attn=self.d_attn)
 
     def call(self, inputs: TensorLike) -> TensorLike:
         u, v = ops.split(inputs, 2, axis=-1)
 
         v = self.norm(v)
-        v = ops.transpose(v)
+        v = ops.transpose(v, (0, 2, 1))
         v = self.proj(v)
-        v = ops.transpose(v)
+        v = ops.transpose(v, (0, 2, 1))
+
+        if self.att is not None:
+            v += self.att(inputs)
 
         return u * v
 
 
-class gMLP(layers.Layer):
-    def __init__(self, input_shape: Tuple[Int], dropout: Float = 0.1, eps: Float = 1e-6, **kwargs):
+class gMLPBlock(layers.Layer):
+    def __init__(self, ffn_dim: Int, dropout: Float = 0.1, eps: Float = 1e-6,
+                 use_attention: bool = False, d_attn: Int = 64, **kwargs):
         super().__init__(**kwargs)
         self.norm = layers.LayerNormalization(epsilon=eps)
 
         self.proj_in = Sequential([
-            layers.Dense(input_shape[-1]*2, activation=activations.gelu),
+            layers.Dense(ffn_dim, activation=activations.gelu),
             layers.Dropout(dropout)
         ])
 
-        self.sgu = SpatialGatingUnit(input_shape[-2])
+        self.sgu = SpatialGatingUnit(eps=eps, use_attention=use_attention, d_attn=d_attn)
 
+        self.proj_out = None
+
+    def build(self, input_shape):
         self.proj_out = layers.Dense(input_shape[-1])
 
     def call(self, inputs: TensorLike) -> TensorLike:
-        inputs = self.norm(inputs)
-        x = self.proj_in(inputs)
+        x = self.norm(inputs)
+        x = self.proj_in(x)
         x = self.sgu(x)
         x = self.proj_out(x)
         return inputs + x
+
+
+class TinyAttention(layers.Layer):
+    def __init__(self, d_out: Int, d_attn: Int = 64, **kwargs):
+        super().__init__(**kwargs)
+        self.d_attn = d_attn
+        self.initial_proj = layers.Dense(3 * d_attn)
+        self.final_proj = layers.Dense(d_out)
+
+    def call(self, inputs: TensorLike) -> TensorLike:
+        x = self.initial_proj(inputs)
+        q, k, v = ops.split(x, 3, axis=-1)
+        w = ops.einsum("bnd,bmd->bnm", q, k)
+        a = ops.softmax(w * ops.rsqrt(self.d_attn))
+        x = ops.einsum("bnm,bmd->bnd", a, v)
+        y = self.final_proj(x)
+        return y
+
+
